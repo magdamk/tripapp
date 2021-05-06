dcfb77020d9a03f18759296cd9f18e72dc2ec99c
magdamk <75847988+magdamk@users.noreply.github.com>
2021-05-06 10:37:41 +0200

    DB setup

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to DB'));
 
 app.listen(3000, () => {
     console.log('TripApp started');
