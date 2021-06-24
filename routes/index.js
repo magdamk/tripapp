8af498c44d91910b5c1d840923a4a1234dffb48a
magdamk <75847988+magdamk@users.noreply.github.com>
2021-06-24 16:47:13 +0200

    cors na routes

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,25 @@ const adminController = require('../controllers/admin_controller');
 //login routes
 router.get('/register/', adminController.getRegister);
 router.post('/register/', adminController.postRegister);
-router.get('/api/admin/', adminController.checkAuthenticated, adminController.getAutLogin);
-router.get('/api/admin/', adminController.checkNotAuthenticated, adminController.getNotAutLogin);
-router.get('/api/logout/', adminController.getLogout);
-router.post('/api/admin/', passport.authenticate('local', { failureFlash: true, failureMessage: "Nieprawidłowe dane logowania" }), adminController.postLogin);
+router.get('/api/admin/', cors(), adminController.checkAuthenticated, adminController.getAutLogin);
+router.get('/api/admin/', cors(), adminController.checkNotAuthenticated, adminController.getNotAutLogin);
+router.get('/api/logout/', cors(), adminController.getLogout);
+router.post('/api/admin/', cors(), passport.authenticate('local', { failureFlash: true, failureMessage: "Nieprawidłowe dane logowania" }), adminController.postLogin);
 
 //place routes
-router.get('/api/places/', placeController.getAllPlaces);
-router.get('/api/places/:id', placeController.getPlaceById, placeController.getPlaceDetailsById);
-router.post('/api/places/', adminController.checkAuthenticated, placeController.createPlace);
-router.patch('/api/places/:id', adminController.checkAuthenticated, placeController.getPlaceById, placeController.updatePlace);
+router.get('/api/places/', cors(), placeController.getAllPlaces);
+router.get('/api/places/:id', cors(), placeController.getPlaceById, placeController.getPlaceDetailsById);
+router.post('/api/places/', cors(), adminController.checkAuthenticated, placeController.createPlace);
+router.patch('/api/places/:id', cors(), adminController.checkAuthenticated, placeController.getPlaceById, placeController.updatePlace);
 
 //photo routes
-router.get('/api/photos/:id', placeController.getPlaceById, photoController.getPhotosForPlace); //place id
-router.post('/api/photos/:id', adminController.checkAuthenticated, placeController.getPlaceById, photoController.addPhotoForPlace); //place id
-router.delete('/api/photos/:id', adminController.checkAuthenticated, photoController.removePhotoById); //photo id
+router.get('/api/photos/:id', cors(), placeController.getPlaceById, photoController.getPhotosForPlace); //place id
+router.post('/api/photos/:id', cors(), adminController.checkAuthenticated, placeController.getPlaceById, photoController.addPhotoForPlace); //place id
+router.delete('/api/photos/:id', cors(), adminController.checkAuthenticated, photoController.removePhotoById); //photo id
 
 //comment routes
-router.get('/api/comments/:id', placeController.getPlaceById, commentController.getCommentsForPlace); //place id
-router.post('/api/comments/:id', placeController.getPlaceById, commentController.createCommentForPlace); //place id
-router.delete('/api/comments/:id', adminController.checkAuthenticated, commentController.markCommentNotProper); //comment id
-
-//weather routes
+router.get('/api/comments/:id', cors(), placeController.getPlaceById, commentController.getCommentsForPlace); //place id
+router.post('/api/comments/:id', cors(), placeController.getPlaceById, commentController.createCommentForPlace); //place id
+router.delete('/api/comments/:id', cors(), adminController.checkAuthenticated, commentController.markCommentNotProper); //comment id
 
 module.exports = router;
